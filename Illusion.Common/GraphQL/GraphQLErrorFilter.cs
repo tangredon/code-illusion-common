b3de9e1c2d2f9dd63e920b924d4bc0202eb651c5
tangredon <9369471+tangredon@users.noreply.github.com>
2020-10-17 22:19:54 +0300

    Add GraphQL basic helpers

---
+++ Illusion.Common/GraphQL/GraphQLErrorFilter.cs
@@ -0,0 +1,14 @@
+﻿using HotChocolate;
+
+namespace Illusion.Service.Storage.GraphQL.Helpers
+{
+    public class GraphQLErrorFilter : IErrorFilter
+    {
+        public IError OnError(IError error)
+        {
+            return error
+                .WithMessage(error.Exception.Message)
+                .WithCode(ErrorInfoProvider.GetErrorCode(error.Exception.GetType()));
+        }
+    }
+}
