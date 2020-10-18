bc7ea87d13a25dd6426b84f10cc10a3f5a2ebbe1
tangredon <9369471+tangredon@users.noreply.github.com>
2020-10-18 16:13:38 +0300

    Add MediatR common behaviors

---
+++ Illusion.Common/MediatR/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Illusion.Common.MediatR.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Illusion.Common.MediatR
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPipelineBehaviors(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
