670f312a34d96f7bc2b994e6f45c8c6c8b7a9710
tangredon <9369471+tangredon@users.noreply.github.com>
2020-12-01 18:07:02 +0200

    Add event deserialize from string

--- Illusion.Common/Framework/Helpers/EventSerializer.cs
+++ Illusion.Common/Framework/Helpers/EventSerializer.cs
@@ -20,5 +20,6 @@ namespace Illusion.Common.Framework.Helpers
         public static string SerializeToString<T>(T @event) => JsonSerializer.Serialize((object)@event, Options);
         public static byte[] SerializeToBytes<T>(T @event) => JsonSerializer.SerializeToUtf8Bytes((object)@event, Options);
         public static T Deserialize<T>(Type eventClrType, in ReadOnlySpan<byte> data) => (T)JsonSerializer.Deserialize(data, eventClrType, Options);
+        public static T Deserialize<T>(Type eventClrType, string data) => (T)JsonSerializer.Deserialize(data, eventClrType, Options);
     }
 }
