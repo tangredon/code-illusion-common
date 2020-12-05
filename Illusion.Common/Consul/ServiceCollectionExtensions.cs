7e2a28aa3a43e028f637ff1654397ec86f96e02a
tangredon <9369471+tangredon@users.noreply.github.com>
2020-12-05 14:35:26 +0200

    Improve Consul Extensions

--- Illusion.Common/Consul/ServiceCollectionExtensions.cs
+++ Illusion.Common/Consul/ServiceCollectionExtensions.cs
@@ -4,29 +4,74 @@ using Consul;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Illusion.Common.Consul
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddConsul(this IServiceCollection services, ConsulSettings settings)
+        public static IServiceCollection AddConsul(this IServiceCollection services)
         {
+            var configuration = services.BuildServiceProvider().GetService<IConfiguration>();
+
+            var settings = new ConsulSettings();
+            var consulSection = configuration.GetSection(ConsulSettings.SectionName);
+
+            consulSection.Bind(settings);
+
             services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
             {
                 var address = settings.Host;
                 consulConfig.Address = new Uri(address);
             }));
+
+            services.Configure<ConsulRegistrationSettings>(consulSection);
+
             return services;
         }
 
-        public static IApplicationBuilder UseConsul(this IApplicationBuilder app, ConsulRegistrationSettings settings)
-        {
-            var consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
+        //private static ConsulRegistrationSettings BuildConsulSettings(IWebHostEnvironment env, IOptions<ConsulRegistrationSettings> consulOptions)
+        //{
+        //    var consul = consulOptions.Value;
+        //    consul.Id = $"{consul.Name}:{Guid.NewGuid()}";
+        //    consul.Meta = new Dictionary<string, string>()
+        //    {
+        //        {"Version", VersionInfo.Version}
+        //    };
 
+        //    if (env.IsDevelopment())
+        //    {
+        //        consul.Tags = new[] { "dev" };
+        //    }
+
+        //    return consul;
+        //}
+
+        public static IApplicationBuilder UseConsul(this IApplicationBuilder app)
+        {
             var logger = app.ApplicationServices.GetRequiredService<ILoggerFactory>().CreateLogger("Consul:Extensions");
+
+            var consulSettings = app.ApplicationServices.GetService<IOptions<ConsulRegistrationSettings>>();
+            if (consulSettings == null)
+            {
+                logger.LogWarning("Could not resolve consul settings");
+                return app;
+            }
+
+            var settings = consulSettings.Value;
+
+            settings.Id = $"{settings.Name}:{Guid.NewGuid()}";
+            //settings.Meta = new Dictionary<string, string>()
+            //{
+            //    {"Version", VersionInfo.Version}
+            //};
+
+            var consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
+            
             var lifetime = app.ApplicationServices.GetRequiredService<IHostApplicationLifetime>();
 
             if (!(app.Properties["server.Features"] is FeatureCollection features)) return app;
