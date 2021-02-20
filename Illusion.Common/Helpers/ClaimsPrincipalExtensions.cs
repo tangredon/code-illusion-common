8e371071ff98692709ea93ecda582187a8cb5ab6
tangredon <9369471+tangredon@users.noreply.github.com>
2021-02-20 22:54:47 +0200

    Add Claims Principal Extensions

---
+++ Illusion.Common/Helpers/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace Illusion.Common.Helpers
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetGuid(this ClaimsPrincipal principal)
+        {
+            if (!(principal.Identity is ClaimsIdentity identity))
+            {
+                throw new Exception("Invalid user identity");
+            }
+
+            var name = identity.Name;
+            if (Guid.TryParse(name, out var guid))
+            {
+                return guid;
+            }
+            else
+            {
+                throw new Exception("Invalid user identifier");
+            }
+        }
+    }
+}
