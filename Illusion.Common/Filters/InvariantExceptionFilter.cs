164dd90701f31e797165b4967d9767cffa008c68
tangredon <9369471+tangredon@users.noreply.github.com>
2021-02-20 20:58:13 +0200

    Add InvariantExceptionFilter

---
+++ Illusion.Common/Filters/InvariantExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Illusion.Common.Framework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Illusion.Common.Filters
+{
+    internal class ExceptionObject
+    {
+        public string Message { get; set; }
+    }
+
+    public class InvariantExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is InvariantValidationException exception)
+            {
+                var exceptionObject = new ExceptionObject
+                {
+                    Message = exception.Message
+                };
+
+                context.Result = new BadRequestObjectResult(exceptionObject);
+            }
+        }
+    }
+}
