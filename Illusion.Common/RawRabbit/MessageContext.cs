7361c11cb3ea207f374c5af5199060074d5fbc44
tangredon <9369471+tangredon@users.noreply.github.com>
2021-02-25 00:57:43 +0200

    Switch to .NET5

---
+++ Illusion.Common/RawRabbit/MessageContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RawRabbit.Enrichers.MessageContext.Context;
+
+namespace Illusion.Common.RawRabbit
+{
+    public class MessageContext : IMessageContext
+    {
+        public Guid GlobalRequestId { get; set; }
+        public string Source { get; set; }
+        public Guid OwnerId { get; set; }
+        public MessageTypeInformation TypeInfo { get; set; }
+        public string RoutingKey { get; set; }
+        public string SpanContext { get; set; }
+    }
+}
