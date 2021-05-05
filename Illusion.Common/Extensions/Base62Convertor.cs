202965f60a5616acb96408e981e9c166d6dbbff7
tangredon <9369471+tangredon@users.noreply.github.com>
2021-05-06 01:25:23 +0300

    Add Base62 Convertor

---
+++ Illusion.Common/Extensions/Base62Convertor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using Base62;
+
+namespace Illusion.Common.Extensions
+{
+    public static class Base62Convertor
+    {
+        public static Guid ConvertFrom(string base62) => new(base62.FromBase62().Reverse().ToArray());
+        public static string ConvertTo(Guid guid) => guid.ToByteArray().Reverse().ToArray().ToBase62();
+    }
+}
