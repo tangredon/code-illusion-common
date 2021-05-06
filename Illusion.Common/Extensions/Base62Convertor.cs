94136846f33b22841c08975344e2b6a58683857a
tangredon <9369471+tangredon@users.noreply.github.com>
2021-05-06 21:52:21 +0300

    Add null check

--- Illusion.Common/Extensions/Base62Convertor.cs
+++ Illusion.Common/Extensions/Base62Convertor.cs
@@ -6,7 +6,24 @@ namespace Illusion.Common.Extensions
 {
     public static class Base62Convertor
     {
-        public static Guid ConvertFrom(string base62) => new(base62.FromBase62(true).ToArray());
-        public static string ConvertTo(Guid guid) => guid.ToByteArray().ToArray().ToBase62(true);
+        public static Guid ConvertFrom(string base62)
+        {
+            if (string.IsNullOrEmpty(base62))
+            {
+                return Guid.Empty;
+            }
+
+            return new Guid(base62.FromBase62(true).ToArray());
+        }
+
+        public static string ConvertTo(Guid guid)
+        {
+            if (guid == Guid.Empty)
+            {
+                return null;
+            }
+
+            return guid.ToByteArray().ToArray().ToBase62(true);
+        }
     }
 }
