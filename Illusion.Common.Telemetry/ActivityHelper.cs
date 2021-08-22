091e2b8f4fb91b1049cdea17633299832ee312a0
tangredon <9369471+tangredon@users.noreply.github.com>
2021-08-22 20:55:40 +0300

    Add Activity Helper

---
+++ Illusion.Common.Telemetry/ActivityHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+namespace Illusion.Common.Telemetry
+{
+    public static class ActivityHelper
+    {
+        public static Activity Current => Activity.Current ?? new Activity("null-activity");
+    }
+}
