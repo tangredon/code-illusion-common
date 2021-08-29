2c728baed59e5a8ed3d8f993f1eae361f37e3014
tangredon <9369471+tangredon@users.noreply.github.com>
2021-08-29 13:10:57 +0300

    Remove old files

--- version.ps1
+++
@@ -1,7 +0,0 @@
-if ($env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME -ne 'tangredon/code-illusion'))
-{ 
-	Write-Host "Skip generating version for external repo"
-	exit
-}
-
-Invoke-Expression "& gitversion /l console /output buildserver /updateAssemblyInfo"
