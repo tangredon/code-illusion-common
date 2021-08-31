902ec69092e805d3b41fdcc685d8749bb2d18779
tangredon <9369471+tangredon@users.noreply.github.com>
2021-08-31 11:47:39 +0300

    Improve GitVersion and MediatR Logging

--- Illusion.Common.MediatR/Behaviors/LoggingBehavior.cs
+++ Illusion.Common.MediatR/Behaviors/LoggingBehavior.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Logging;
@@ -10,22 +7,27 @@ namespace Illusion.Common.MediatR.Behaviors
 {
     internal class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
-        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
-        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        private readonly ILogger _logger;
+        public LoggingBehavior(ILogger<TRequest> logger)
         {
             _logger = logger;
         }
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             //Request
-            _logger.LogInformation($"Handling {typeof(TRequest).Name}");
-            Type myType = request.GetType();
-            IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
-            foreach (PropertyInfo prop in props)
-            {
-                object propValue = prop.GetValue(request, null);
-                _logger.LogInformation("{Property} : {@Value}", prop.Name, propValue);
-            }
+            //_logger.LogInformation($"Handling {typeof(TRequest).Name}");
+            //Type myType = request.GetType();
+            //IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+            //foreach (PropertyInfo prop in props)
+            //{
+            //    object propValue = prop.GetValue(request, null);
+            //    _logger.LogInformation("{Property} : {@Value}", prop.Name, propValue);
+            //}
+
+            var name = typeof(TRequest).Name;
+
+            _logger.LogInformation("Handling Request: {Name} {@Request}", name, request);
+
             var response = await next();
             //Response
             _logger.LogInformation($"Handled {typeof(TResponse).Name}");
