f9084c3130874c3491d881c75ae4ce4aac7fc2e3
tangredon <9369471+tangredon@users.noreply.github.com>
2021-09-03 16:39:36 +0300

    Remove consumer context

--- Illusion.Common.RabbitMq/IllusionMessageContext.cs
+++ Illusion.Common.RabbitMq/IllusionMessageContext.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using RabbitMQ.Client;
-using RawRabbit.Common;
 using RawRabbit.Enrichers.MessageContext.Context;
 
 namespace Illusion.Common.RabbitMq
@@ -8,13 +6,8 @@ namespace Illusion.Common.RabbitMq
     public class IllusionMessageContext : IMessageContext
     {
         public Guid GlobalRequestId { get; set; }
-        public RetryInformation RetryInfo { get; set; }
+        public string SourceTraceId { get; set; }
         public string TraceId { get; set; }
         public string SpanId { get; set; }
-    }
-
-    public class ConsumerIllusionMessageContext : IllusionMessageContext
-    {
-        public IBasicProperties BasicProperties { get; set; }
     }
 }
